adca707af234452fb1fc43bcaf9c6efdd238bf20
Alla <allasamoilenko@gmx.de>
2024-11-11 20:30:00 +0200

    Base2

--- main.py
+++ main.py
@@ -24,10 +24,25 @@ class SinglyLinkedList:
             current = current.next
         print("None")
 
+    def delete(self, del_data):
+        current = self.head
+        if current and current.data == del_data:
+            self.head = current.next
+            return
+        k = None
+        while current and current.data != del_data:
+            k = current
+            current = current.next
+        if current is None:
+            return
+        k.next = current.next
+
 sll = SinglyLinkedList()
 sll.append(1)
 sll.append(2)
 sll.append(4)
+sll.display()
+sll.delete(2)
 
 sll.display()
 
@@ -67,3 +82,36 @@ dll.append(10)
 
 dll.display()
 
+class Node3:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.top = None
+    def push(self, data):
+        new_node = Node3(data)
+        new_node.next = self.top
+        self.top = new_node
+    def pop(self):
+        if not self.top:
+            return None
+        data = self.top.data
+        self.top = self.top.next
+        return data
+    def display(self):
+        current = self.top
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        print("None")
+
+stack = Stack()
+stack.push(4)
+stack.push(7)
+stack.push(9)
+stack.push(5)
+stack.push(3)
+stack.display()
+print(stack.pop())
+stack.display()

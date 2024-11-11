192c1fb3754642192be49a646f03ad1593c169aa
Alla <allasamoilenko@gmx.de>
2024-11-11 20:06:40 +0200

    Base

--- main.py
+++ main.py
@@ -1,1 +1,69 @@
-print("Hello")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        print("None")
+
+sll = SinglyLinkedList()
+sll.append(1)
+sll.append(2)
+sll.append(4)
+
+sll.display()
+
+class Node_2:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+        new_node.prev = last
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        print("None")
+
+dll = DoublyLinkedList()
+
+dll.append(7)
+dll.append(8)
+dll.append(7)
+dll.append(9)
+dll.append(10)
+
+dll.display()
+
